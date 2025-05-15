f9ac2446e01c5beba388b43f75e8ad7b050d8da2
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2025-05-15 18:13:46 +0200

    idk

--- ui/BuildHUD.java
+++ ui/BuildHUD.java
@@ -1,32 +1,27 @@
 package ui;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class BuildHUD extends AbstractHUD {
 
-    private final JButton startWaveButton;
-    private final JButton openShopButton;
+    private JButton shopButton;
+    private JButton startWaveButton;
 
     public BuildHUD() {
         super();
 
+        this.shopButton = new JButton("Shop");
         this.startWaveButton = new JButton("Start Wave");
-        this.startWaveButton.setBounds(10, 600, 200, 75);
-        this.startWaveButton.setFont(new Font("Arial", Font.BOLD, 30));
-        add(this.startWaveButton);
 
-        this.openShopButton = new JButton("Shop");
-        this.openShopButton.setBounds(1055, 600, 200, 75);
-        this.openShopButton.setFont(new Font("Arial", Font.BOLD, 30));
-        add(this.openShopButton);
+        add(this.shopButton);
+        add(this.startWaveButton);
+    }
+
+    public JButton getShopButton() {
+        return this.shopButton;
     }
 
     public JButton getStartWaveButton() {
         return this.startWaveButton;
-    }
-
-    public JButton getOpenShopButton() {
-        return this.openShopButton;
     }
 }
